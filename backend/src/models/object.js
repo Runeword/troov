4461890689a81f188c708012616de8b4badde8f7
Runeword <60324746+Runeword@users.noreply.github.com>
2024-12-31 22:17:16 +0100

    feat(objects): add the object model

---
+++ backend/src/models/object.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const objectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Object", objectSchema);
