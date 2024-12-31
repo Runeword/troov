01399341417f5d80d05664a00baa8670a025db27
Runeword <60324746+Runeword@users.noreply.github.com>
2024-12-31 10:42:46 +0100

    feat(flake.nix): use nix to install project dependencies

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = nixpkgs.lib.systems.flakeExposed;
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.typescript
+              pkgs.nodejs_23
+              pkgs.deno
+              pkgs.nodePackages.pnpm
+            ];
+          };
+        }
+      );
+    };
+}
